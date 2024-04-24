5f1afc9ad41d6cd63ae4721d7980b111fc2dfa59
Yusuf Abdulkarim <haywhyze@gmail.com>
2024-04-24 07:51:08 +0100

    Refactor TributesPage component to sort tributes by created date

--- app/tributes/page.tsx
+++ app/tributes/page.tsx
@@ -4,6 +4,15 @@ import { formatDistanceToNow } from 'date-fns';
 import { seed } from '@/lib/seed';
 
 export const dynamic = 'force-dynamic'
+
+interface Tribute {
+  id: number;
+  name: string;
+  relationship: string;
+  message: string;
+  created_at: string;
+}
+
 const TributesPage: React.FC = async () => {
   let data: any;
 
@@ -24,14 +33,19 @@ const TributesPage: React.FC = async () => {
 
   const { rows: tributes } = data;
 
+  tributes.sort(
+    (a: any, b: any) =>
+      new Date(b.created_at).getTime() - new Date(a.created_at).getTime()
+  );
+
   return (
-    <main className='p-4'>
+    <main className='p-4 min-h-screen'>
       <h1 className='text-center text-4xl font-bold my-6'>Tributes</h1>
-      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 min-h-screen'>
-        {tributes.map((tribute: any) => (
+      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'>
+        {tributes.map((tribute: Tribute) => (
           <div
             key={tribute.id}
-            className='max-w-sm rounded overflow-hidden shadow-lg p-4'
+            className='max-w-sm bg-white rounded-lg overflow-hidden shadow-lg p-4'
           >
             <h2 className='font-bold text-xl mb-2'>{tribute.name}</h2>
             <p className='italic text-lg'>{tribute.relationship}</p>
